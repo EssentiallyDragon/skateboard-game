046905241050c428a668a316c95a670459a991f7
TidalTreka <150077624+TidalTreka@users.noreply.github.com>
2023-11-08 14:56:54 -0500

    Update Node2D.gd

--- Node2D.gd
+++ Node2D.gd
@@ -5,7 +5,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if not airborn:
-		skate_speed + skate_speed * 5.10# Replace with function body.
+		skate_speed + skate_speed * 0.10# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
